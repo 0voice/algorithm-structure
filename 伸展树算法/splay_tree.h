32c33976a99260f34a8e9a1289ea655c4d9c4873
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-10 11:39:59 +0800

    Update splay_tree.h

--- 伸展树算法/splay_tree.h
+++ 伸展树算法/splay_tree.h
@@ -1,1 +1,25 @@
-#
+#ifndef _SPLAY_TREE_H
+#define _SPLAY_TREE_H
+
+struct Node;
+typedef int ElementType;
+typedef struct Node *PtrToNode;
+typedef PtrToNode SplayTree;
+typedef PtrToNode Position;
+
+SplayTree MakeEmpty(SplayTree T);
+Position Find(ElementType X,SplayTree T);
+Position FindMax(SplayTree T);
+Position FindMin(SplayTree T);
+SplayTree Insert(ElementType X,SplayTree T);
+SplayTree Delete(ElementType X,SplayTree T);
+
+#endif
+
+struct Node
+{
+    ElementType element;
+    PtrToNode Left;
+    PtrToNode Right;
+    PtrToNode Parent;
+};

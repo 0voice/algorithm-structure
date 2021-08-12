900abc32d1e709233ffa331ed8d3bdfaac77fc53
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-12 20:49:58 +0800

    Create 在字符串的指定位置插入字符.c

---
+++ 经典项目/在字符串的指定位置插入字符.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int fun(char *s)
+{
+    char t[80];
+    int i, j;
+    for(i=0; s[i]; i++)  /*将串s拷贝至串t*/
+        t[i]=s[i];
+    t[i]='\0';
+    for(i=0,j=0; t[i]; i++)
+    /*对于数字字符先写一个$符号，再写该数字字符*/
+        if(t[i]>='0' && t[i]<='9')
+        {
+            s[j++]='$';
+            s[j++]=t[i];
+        }
+    /*对于非数字字符原样写入串s*/
+        else
+            s[j++]=t[i];
+    s[j]='\0';  /*在串s结尾加结束标志*/
+    return 0;
+}
+int main()
+{
+    char s[80];
+    printf ( "Enter a string:" );
+    scanf ("%s", s);  /*输入字符串*/
+    fun(s);
+    printf ("The result: %s\n", s);  /*输出结果*/
+    return 0;
+}
+
+//运行结果：
+
+//Enter a string:A1B23CD45
+//The result: A$1B$2$3CD$4$5

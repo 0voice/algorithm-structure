ce9e70a3c68c2194e6a79a856a8463d7260d0166
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-12 20:44:56 +0800

    Create 递归解决分鱼问题.c

---
+++ 经典项目/递归解决分鱼问题.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/*分鱼递归函数*/
+int fish(int n, int x)
+{
+    if((x-1)%5 == 0)
+    {
+        if(n==1)
+            return 1;  /*递归出口*/
+        else
+            return fish(n-1, (x-1)/5*4);  /*递归调用*/
+    }
+    return 0;  /*x不是符合题意的解，返回0*/
+}
+int main()
+{
+    int fish[6], i;
+    fish[5]=6;
+    while(1)
+    {
+        for(i=4; i>0; i--)
+        {
+            if(fish[i+1]%4!=0)
+                break;
+            fish[i]=fish[i+1]*5/4+1;
+            if(fish[i]%5!=1)
+                break;
+        }
+        if(i == 0)
+            break;
+        fish[5]+=5;
+    }
+    for(i=1; i<=5; i++)
+        printf("fish[%d]=%d\n", i, fish[i]);
+    return 0;
+}

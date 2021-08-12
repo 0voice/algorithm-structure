c32608a01b70517501f0ad43d552527db46aa859
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-12 17:10:24 +0800

    Update 并查集.c

--- 并查集/并查集.c
+++ 并查集/并查集.c
@@ -1,1 +1,68 @@
-#
+#include <cstdio>
+#include <cstring>
+#define MAXN 1005
+typedef long long ll;
+int fa[MAXN], rank[MAXN];
+ll X[MAXN], Y[MAXN], Z[MAXN];
+inline bool next_to(ll x1, ll y1, ll z1, ll x2, ll y2, ll z2, ll r)
+{
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2) <= 4 * r * r;
+    //判断两个空洞是否相交或相切
+}
+inline void init(int n)
+{
+    for (int i = 1; i <= n; ++i)
+    {
+        fa[i] = i;
+        rank[i] = 1;
+    }
+}
+int find(int x)
+{
+    return x == fa[x] ? x : (fa[x] = find(fa[x]));
+}
+inline void merge(int i, int j)
+{
+    int x = find(i), y = find(j);
+    if (rank[x] <= rank[y])
+        fa[x] = y;
+    else
+        fa[y] = x;
+    if (rank[x] == rank[y] && x != y)
+        rank[y]++;
+}
+int main()
+{
+    int T, n, h;
+    ll r;
+    scanf("%d", &T);
+    for (int I = 0; I < T; ++I)
+    {
+        memset(X, 0, sizeof(X));
+        memset(Y, 0, sizeof(Y));
+        memset(Z, 0, sizeof(Z));
+        scanf("%d%d%lld", &n, &h, &r);
+        init(n);
+        fa[1001] = 1001; //用1001代表底部
+        fa[1002] = 1002; //用1002代表顶部
+        for (int i = 1; i <= n; ++i)
+            scanf("%lld%lld%lld", X + i, Y + i, Z + i);
+        for (int i = 1; i <= n; ++i)
+        {
+            if (Z[i] <= r)
+                merge(i, 1001); //与底部接触的空洞与底部合并
+            if (Z[i] + r >= h)
+                merge(i, 1002); //与顶部接触的空洞与顶部合并
+        }
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = i + 1; j <= n; ++j)
+            {
+                if (next_to(X[i], Y[i], Z[i], X[j], Y[j], Z[j], r))
+                    merge(i, j); //遍历所有空洞，合并相交或相切的球
+            }
+        }
+        printf("%s\n", find(1001) == find(1002) ? "Yes" : "No");
+    }
+    return 0;
+}

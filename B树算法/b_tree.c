024a5efb3c1d3b3396b54a19a7cf7951142a3316
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-12 17:24:27 +0800

    Update b_tree.c

--- B树算法/b_tree.c
+++ B树算法/b_tree.c
@@ -1,1 +1,39 @@
-#
+// Function to traverse all nodes in a subtree rooted with this node
+void BTreeNode::traverse()
+{
+    // There are n keys and n+1 children, travers through n keys
+    // and first n children
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        // If this is not leaf, then before printing key[i],
+        // traverse the subtree rooted with child C[i].
+        if (leaf == false)
+            C[i]->traverse();
+        cout << " " << keys[i];
+    }
+ 
+    // Print the subtree rooted with last child
+    if (leaf == false)
+        C[i]->traverse();
+}
+
+// Function to search key k in subtree rooted with this node
+BTreeNode *BTreeNode::search(int k)
+{
+    // Find the first key greater than or equal to k
+    int i = 0;
+    while (i < n && k > keys[i])
+        i++;
+ 
+    // If the found key is equal to k, return this node
+    if (keys[i] == k)
+        return this;
+ 
+    // If key is not found here and this is a leaf node
+    if (leaf == true)
+        return NULL;
+ 
+    // Go to the appropriate child
+    return C[i]->search(k);
+}

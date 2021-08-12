1cf52874d5146cdb8ab4f62b327d2941c913f585
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-12 20:46:04 +0800

    Create 递归解决年龄问题.c

---
+++ 经典项目/递归解决年龄问题.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int age(int n)
+{
+    int x;
+    if(n == 1)
+        x=10;
+    else
+        x=age(n-1)+2;
+    return x;
+}
+int main()
+{
+    int n;
+    printf("请输入n值：");
+    scanf("%d", &n);
+    printf("第%d个人的年龄为%d\n", n, age(n));
+    return 0;
+}

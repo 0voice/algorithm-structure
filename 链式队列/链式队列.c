6b7b2627d1fc419edf00113460464521bfb62310
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-12 10:54:11 +0800

    Update 链式队列.c

--- 链式队列/链式队列.c
+++ 链式队列/链式队列.c
@@ -1,1 +1,50 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct QNode{
+    int data;
+    struct QNode * next;
+}QNode;
+QNode * initQueue(){
+    QNode * queue=(QNode*)malloc(sizeof(QNode));
+    queue->next=NULL;
+    return queue;
+}
+QNode* enQueue(QNode * rear,int data){
+    QNode * enElem=(QNode*)malloc(sizeof(QNode));
+    enElem->data=data;
+    enElem->next=NULL;
+    //使用尾插法向链队列中添加数据元素
+    rear->next=enElem;
+    rear=enElem;
+    return rear;
+}
+QNode* DeQueue(QNode * top,QNode * rear){
+    if (top->next==NULL) {
+        printf("\n队列为空");
+        return rear;
+    }
+    QNode * p=top->next;
+    printf("%d ",p->data);
+    top->next=p->next;
+    if (rear==p) {
+        rear=top;
+    }
+    free(p);
+    return rear;
+}
+int main() {
+    QNode * queue,*top,*rear;
+    queue=top=rear=initQueue();//创建头结点
+    //向链队列中添加结点，使用尾插法添加的同时，队尾指针需要指向链表的最后一个元素
+    rear=enQueue(rear, 1);
+    rear=enQueue(rear, 2);
+    rear=enQueue(rear, 3);
+    rear=enQueue(rear, 4);
+    //入队完成，所有数据元素开始出队列
+    rear=DeQueue(top, rear);
+    rear=DeQueue(top, rear);
+    rear=DeQueue(top, rear);
+    rear=DeQueue(top, rear);
+    rear=DeQueue(top, rear);
+    return 0;
+}

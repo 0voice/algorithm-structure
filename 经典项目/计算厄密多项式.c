7abda625f850b54beb826b8c92d0145fc8fa4365
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-08-21 15:07:47 +0800

    Update 计算厄密多项式.c

--- 经典项目/计算厄密多项式.c
+++ 经典项目/计算厄密多项式.c
@@ -1,1 +1,30 @@
-#
+#include <stdio.h>
+
+/**
+ * 计算 Hermite polynomial 的值
+ *
+ * 输入：
+ *    n, x：用于标识值
+ *
+ * 输出：
+ *    polynomial的值（返回值）
+**/
+
+int hermite( int n, int x )
+{
+    // 处理不需要递归的特殊情况。
+    if( n <= 0 )
+        return 1;
+    if( n == 1 )
+        return 2 * x;
+
+    // 否则，递归地计算结果值。
+    return 2 * x * hermite( n - l, x ) - 2 * ( n - 1 ) * hermite ( n - 2, x );
+}
+
+int main()
+{
+    printf("%d\n", hermite(3,2));
+    
+    return 0;
+}
